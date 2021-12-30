8c0d6a03c939b7479966f9a8e4c924ef68cd4dcb
Joey Lim <jyorin0206@gmail.com>
2021-12-30 22:13:48 +0800

    built ui for ReadingScreen.kt

--- app/src/main/java/com/example/clickcap/screens/ReadingScreen.kt
+++ app/src/main/java/com/example/clickcap/screens/ReadingScreen.kt
@@ -1,9 +1,44 @@
 package com.example.clickcap.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.clickcap.composables.ClickAppBar
 
 @Composable
-fun ReadingScreen(navController: NavController) {
+fun ReadingScreen(navController: NavController, currentEye: String = "LEFT", readingIndex: Int = 1) {
+    val currentReading by remember { mutableStateOf(0.0) }
+    Scaffold(
+        topBar = { ClickAppBar() }
+    ) {
+        Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+            Text(text = "Reading $readingIndex", fontSize = 27.sp)
+            Text(text = "$currentEye EYE", fontSize = 20.sp)
+            Text("$currentReading", fontSize = 117.sp)
+            Text(text = "Current Reading")
+            Spacer(modifier = Modifier.height(25.dp))
+
+            // go next action
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "NEXT")
+            }
+            Spacer(modifier = Modifier.height(25.dp))
+
+            // go back to starting point
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Restart")
+            }
+        }
+    }
 
 }
